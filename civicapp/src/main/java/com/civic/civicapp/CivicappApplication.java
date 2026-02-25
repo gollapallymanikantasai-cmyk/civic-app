b3dfb2a56888cbc196357c8f0d64f4a967ebd6be
gollapallymanikantasai-cmyk <gollapallymanikantasai@gmail.com>
2026-02-25 12:06:54 +0530

    Initial Spring Boot Civic project setup

---
+++ civicapp/src/main/java/com/civic/civicapp/CivicappApplication.java
@@ -0,0 +1,13 @@
+package com.civic.civicapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivicappApplication.class, args);
+	}
+
+}
---
+++ civicapp/src/test/java/com/civic/civicapp/CivicappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civic.civicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
