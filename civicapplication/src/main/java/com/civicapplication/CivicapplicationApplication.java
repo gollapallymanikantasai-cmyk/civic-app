a674fa20d4742c7fcb192334645528b04f5cd2ed
gollapallymanikantasai-cmyk <gollapallymanikantasai@gmail.com>
2026-05-05 11:00:32 +0530

    Initial Commit

---
+++ civicapplication/src/main/java/com/civicapplication/CivicapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.civicapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivicapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivicapplicationApplication.class, args);
+	}
+
+}
---
+++ civicapplication/src/test/java/com/civicapplication/CivicapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civicapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
